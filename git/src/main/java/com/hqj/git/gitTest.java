60967f5864ff65dc16590cf7ea7a9b5bdbd540e2
huqingjie <huqingjieq@163.com>
2019-03-21 17:57:32 +0800

    idea上传项目

---
+++ git/src/main/java/com/hqj/git/gitTest.java
@@ -0,0 +1,13 @@
+package com.hqj.git;
+
+public class gitTest {
+    public static void main(String[] args) {
+        System.out.println("aaaaaa");
+        show();
+
+    }
+
+    public static void show(){
+        System.out.println(123456);
+    }
+}
